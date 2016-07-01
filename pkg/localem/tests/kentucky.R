8334655d5c8b6005fe49e236ccc882e2192ea0c9
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-01 20:27:53 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/localem@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/localem/tests/kentucky.R
+++ pkg/localem/tests/kentucky.R
@@ -1,3 +1,15 @@
-library('localem')
+library('localEM')
+
 data('kentuckyCounty') 
 data('kentuckyTract')
+
+
+lemRaster = rasterPartition(
+		polyCoarse = kentuckyCounty, 
+		polyFine = kentuckyTract, 
+    cellsCoarse = 20, cellsFine = 200, 
+    bw = c(10, 20) * 1000, 
+    ncores = 2, 
+    idFile = 'id.grd', 
+		offsetFile = 'offset.grd'
+)
