87af4148dc31cffa5d2ee85cd323f3955c6a896d
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-02 00:00:49 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/localem@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/localem/tests/kentucky.R
+++ pkg/localem/tests/kentucky.R
@@ -3,13 +3,20 @@ library('localEM')
 data('kentuckyCounty') 
 data('kentuckyTract')
 
+ncores = 1+ (.Platform$OS.type=='unix')
 
 lemRaster = rasterPartition(
 		polyCoarse = kentuckyCounty, 
 		polyFine = kentuckyTract, 
     cellsCoarse = 20, cellsFine = 200, 
     bw = c(10, 20) * 1000, 
-    ncores = 2, 
+    ncores = ncores, 
     idFile = 'id.grd', 
 		offsetFile = 'offset.grd'
 )
+
+
+lemSmoothMat = smoothingMatrix(
+		rasterObjects = lemRaster, 
+    ncores = ncores) 
+		
