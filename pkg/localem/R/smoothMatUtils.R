10d0c9b85465defffde4b250ad8a373856af8053
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-07-07 02:11:03 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/localem@171 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/localem/R/smoothMatUtils.R
+++ pkg/localem/R/smoothMatUtils.R
@@ -1,77 +1,77 @@
 # Retrieves the cell information of the rasters of the coarse and fine polygons, and offsets
 getCellInfo = function(
-  cell1, coarse,
-  fine, offsetRaster
+    cell1, coarse,
+    fine, offsetRaster
 ) {
   
   lims=drop(xyFromCell(coarse,cell1))
   resHalf = raster::res(coarse)/2
   
   theextent = extent(c(
-      lims['x']+c(-1,1)*resHalf[1],
-      lims['y']+c(-1,1)*resHalf[2]
-    ))
+          lims['x']+c(-1,1)*resHalf[1],
+          lims['y']+c(-1,1)*resHalf[2]
+      ))
   
   Scells = cellsFromExtent(fine,theextent)
   
   Scols = sort(unique(
-      colFromCell(fine, Scells)
-    ))
+          colFromCell(fine, Scells)
+      ))
   Mcol = min(Scols)
   Ncols = length(Scols)
   Srows = sort(unique(
-      rowFromCell(fine, Scells)
-    ))
+          rowFromCell(fine, Scells)
+      ))
   Mrow = min(Srows)
   Nrows = length(Srows)
   
   id = getValuesBlock(fine,
-    row=Mrow, nrows=Nrows,
-    col=Mcol,ncols=Ncols)
+      row=Mrow, nrows=Nrows,
+      col=Mcol,ncols=Ncols)
   id = cbind(
-    factorValues(fine, id), 
-    index = 1:length(id)
+      factorValues(fine, id), 
+      index = 1:length(id)
   )
   
   theoffset = getValuesBlock(offsetRaster,
-    row=Mrow, nrows=Nrows,col=Mcol,ncols=Ncols)
+      row=Mrow, nrows=Nrows,col=Mcol,ncols=Ncols)
   
   
   partitionDf = as.data.frame(table(
-      fine=id[,'idFine'], 
-      coarse=id[,'idCoarse'])
+          fine=id[,'idFine'], 
+          coarse=id[,'idCoarse'])
   )
   partitionDf = partitionDf[partitionDf$Freq > 0, , drop=FALSE]
   
   list(
-    extent = theextent,
-    Scells=Scells,
-    Mcol=Mcol,Ncols=Ncols,Mrow=Mrow,Nrows=Nrows,
-    Ncells = Ncols*Nrows,
-    id=id,offset=theoffset,
-    idCoarse=sort(unique(id[,'idCoarse'])),
-    partitions = partitionDf,
-    points = xyFromCell(fine, Scells)
+      extent = theextent,
+      Scells=Scells,
+      Mcol=Mcol,Ncols=Ncols,Mrow=Mrow,Nrows=Nrows,
+      Ncells = Ncols*Nrows,
+      id=id,offset=theoffset,
+      idCoarse=sort(unique(id[,'idCoarse'])),
+      partitions = partitionDf,
+      points = xyFromCell(fine, Scells)
   )
 }
 
 # Computes the entries of the smoothing matrix for the partitions
 smoothingMatrixEntries = function(
-  cell1,
-  cell2=cell1,
-  offsetRaster,
-  fine,
-  coarse,
-  focalList
+    cell1,
+    cell2=cell1,
+    offsetRaster,
+    fine,
+    coarse,
+    focalList
 ) {
   if(is.list(focalList)) {
     # focal is not an array, it's a list of focal objects
     # compute the kernel array
     kernelArray = kernMat(
-      cell2, cell1=cell1,
-      focalList=focalList,
-      coarse = coarse,
-      fineRelativeRes = fine,reorder=FALSE)
+        cell2, cell1=cell1,
+        focalList=focalList,
+        coarse = coarse,
+        fineRelativeRes = fine,reorder=FALSE)
   } else {
     # a kernel array was supplied
     kernelArray = focalList
@@ -81,8 +81,8 @@ smoothingMatrixEntries = function(
   offsetBwNames = grep("^bw", names(offsetRaster), value=TRUE)
   # extract data for extent of coarse cells from fine rasters
   cellInfo1=getCellInfo(
-    cell1, coarse,
-    fine, offsetRaster)
+      cell1, coarse,
+      fine, offsetRaster)
   
   if(!length(cellInfo1$idCoarse)){
     return(NULL)
@@ -106,23 +106,23 @@ smoothingMatrixEntries = function(
       thisKernelArray = kernelArray
     } else {
       cellInfo2=getCellInfo(
-        Dcell2,
-        coarse,
-        fine, offsetRaster)
+          Dcell2,
+          coarse,
+          fine, offsetRaster)
       
       # deal with the reordering stuff
       if(is.function(kernelArray)){
         thisDist = xyFromCell(coarse, Dcell2) -
-          xyFromCell(coarse, cell1)
+            xyFromCell(coarse, cell1)
         xoryaxis = c('y','x')[1 +
-            (abs(thisDist[1]) >= abs(thisDist[2]))
+                (abs(thisDist[1]) >= abs(thisDist[2]))
         ]
         quadrant = 1+
-          (thisDist[2]<0) +
-          (thisDist[1]<0) +
-          2*(thisDist[1]<0 & thisDist[2]>=0)
+            (thisDist[2]<0) +
+            (thisDist[1]<0) +
+            2*(thisDist[1]<0 & thisDist[2]>=0)
         thisKernelArray = kernelArray(
-          paste('q', quadrant, xoryaxis, sep='')
+            paste('q', quadrant, xoryaxis, sep='')
         )
       } else {
         thisKernelArray = kernelArray
@@ -140,57 +140,57 @@ smoothingMatrixEntries = function(
     
     smoothedOffset2 = cellInfo2$offset[, offsetBwNames, drop=FALSE]
     smoothedOffset2 = array(smoothedOffset2,
-      c(dim(smoothedOffset2),cellInfo1$Ncells),
-      dimnames = c(dimnames(smoothedOffset2), 
-        list(1:cellInfo1$Ncells)))
+        c(dim(smoothedOffset2),cellInfo1$Ncells),
+        dimnames = c(dimnames(smoothedOffset2), 
+            list(1:cellInfo1$Ncells)))
     smoothedOffset2 = aperm(smoothedOffset2, c(3,1,2))
     
     # the smoothing matrix (or part thereof)
     theDimnames = list(
-      paste("c", cell1, "p", 
-        cellInfo1$partition$coarse, '.', 
-        cellInfo1$partition$fine, sep=''),
-      paste("c", Dcell2, "p",						
-        cellInfo2$partition$coarse, '.', 
-        cellInfo2$partition$fine, sep=''),
-      dimnames(smoothedOffset2)[[3]],
-      c('straightup', 'transpose')
+        paste("c", cell1, "p", 
+            cellInfo1$partition$coarse, '.', 
+            cellInfo1$partition$fine, sep=''),
+        paste("c", Dcell2, "p",						
+            cellInfo2$partition$coarse, '.', 
+            cellInfo2$partition$fine, sep=''),
+        dimnames(smoothedOffset2)[[3]],
+        c('straightup', 'transpose')
     )
     
     
     smoothingMatrix = array(NA, 
-      unlist(lapply(theDimnames, length)),
-      dimnames=theDimnames
+        unlist(lapply(theDimnames, length)),
+        dimnames=theDimnames
     )
     smoothingMatrixOne = array(NA, 
-      dim(smoothingMatrix)[-4],
-      dimnames=theDimnames[-4]
+        dim(smoothingMatrix)[-4],
+        dimnames=theDimnames[-4]
     )
     
     for(D1 in 1:dim(smoothingMatrix)[1]){
       inD1 = which(
-        (cellInfo1$id[,'idCoarse']==cellInfo1$partition[D1,'coarse']) &
-          (cellInfo1$id[,'idFine']==cellInfo1$partition[D1,'fine'])
+          (cellInfo1$id[,'idCoarse']==cellInfo1$partition[D1,'coarse']) &
+              (cellInfo1$id[,'idFine']==cellInfo1$partition[D1,'fine'])
       )
       smoothedOffset1 = cellInfo1$offset[, offsetBwNames, drop=FALSE]
       smoothedOffset1 = array(smoothedOffset1,
-        c(dim(smoothedOffset1),cellInfo2$Ncells))
+          c(dim(smoothedOffset1),cellInfo2$Ncells))
       smoothedOffset1 = aperm(smoothedOffset1,c(1,3,2))
       
       for(D2 in 1:dim(smoothingMatrix)[2]){
         inD2 = which(
-          (cellInfo2$id[,'idCoarse']==cellInfo2$partition[D2,'coarse']) &
-            (cellInfo2$id[,'idFine']==cellInfo2$partition[D2,'fine'])
+            (cellInfo2$id[,'idCoarse']==cellInfo2$partition[D2,'coarse']) &
+                (cellInfo2$id[,'idFine']==cellInfo2$partition[D2,'fine'])
         )
         
         numerator = thisKernelArray[inD1, inD2,,drop=FALSE]
         smoothingMatrixOne[D1,D2,] = apply(
-          numerator/smoothedOffset2[1:length(inD1),inD2,,drop=FALSE], 
-          3, sum) * Scw2 / Sarea1[D1]
+            numerator/smoothedOffset2[1:length(inD1),inD2,,drop=FALSE], 
+            3, sum) * Scw2 / Sarea1[D1]
         if(!cellsAreEqual){
           smoothingMatrix[D1,D2,,'transpose'] =	apply(
-            numerator/smoothedOffset1[inD1,1:length(inD2),,drop=FALSE], 
-            3, sum) * Scw2 / Sarea2[D2]
+              numerator/smoothedOffset1[inD1,1:length(inD2),,drop=FALSE], 
+              3, sum) * Scw2 / Sarea2[D2]
         } #cells Equal
       } #loop Spart22
     } # loop Spart1
@@ -214,20 +214,20 @@ smoothingMatrixEntries = function(
 # Specifically, the computations are done for the same partitions
 # Generates also the matrix that matches the IDs of the coarse polygons with the partitions
 smoothingMatrixDiag = function(
-  rasterCoarse,rasterFine,
-  focalList,offsetRaster, 
-  filename,
-  verbose=FALSE
+    rasterCoarse,rasterFine,
+    focalList,offsetRaster, 
+    filename,
+    verbose=FALSE
 ) {
   
   kernelArrayD = kernMat(
-    cellDist=c(0,0),
-    focalList=focalList,
-    fineRelativeRes=rasterFine,
-    cell1=c(0,0),
-    coarse=rasterCoarse,
-    reorder=FALSE,
-    xv = names(offsetRaster))
+      cellDist=c(0,0),
+      focalList=focalList,
+      fineRelativeRes=rasterFine,
+      cell1=c(0,0),
+      coarse=rasterCoarse,
+      reorder=FALSE,
+      xv = names(offsetRaster))
   
   
   # order the partitions
@@ -241,9 +241,9 @@ smoothingMatrixDiag = function(
   Sbw = dimnames(kernelArrayD)[[3]]
   
   smoothingRasterTemplate = brick(
-    nrows=Npartitions, ncols=Npartitions, 
-    xmn=0,xmx=1,ymn=0,ymx=1,
-    nl = Nsmooths
+      nrows=Npartitions, ncols=Npartitions, 
+      xmn=0,xmx=1,ymn=0,ymx=1,
+      nl = Nsmooths
   )
   names(smoothingRasterTemplate) = Sbw		
   
@@ -255,12 +255,12 @@ smoothingMatrixDiag = function(
   if(verbose) cat("creating raster to hold smoothing matrix\n")
   
   firstRaster = writeStart(
-    smoothingRasterTemplate,
-    filename = firstFile, 
-    format = 'raster',
-    datatype = 'FLT8S', 
-    bandorder = 'BSQ',
-    overwrite=file.exists(firstFile))
+      smoothingRasterTemplate,
+      filename = firstFile, 
+      format = 'raster',
+      datatype = 'FLT8S', 
+      bandorder = 'BSQ',
+      overwrite=file.exists(firstFile))
   
   for(Drow in 1:nrow(firstRaster)) {
     firstRaster = writeValues(firstRaster, toWrite, Drow) 
@@ -270,50 +270,59 @@ smoothingMatrixDiag = function(
   
   # theType = mmap::real64();dput(theType, '')
   theType = structure(numeric(0), bytes = 8L, signed = 1L, class = c("Ctype", 
-      "double"))
+          "double"))
   
   if(verbose) cat("looping through diagonal cells\n")
   
   Dcell1 = NULL # defined for package check 
   diagBlocks = foreach::foreach(
-      Dcell1 = 1:ncell(rasterCoarse), .packages='localEM', .export = 'smoothingMatrixEntries'
-    ) %dopar% {
-      
+          Dcell1 = 1:ncell(rasterCoarse), 
+          .packages=c('raster','Matrix','localEM'), 
+          .export = 'smoothingMatrixEntries'
+      ) %dopar% {
+        
 # for(Dcell1 in 1:ncell(rasterCoarse)) {     
-      thisBlock = smoothingMatrixEntries(cell1 = Dcell1,
-        focalList=kernelArrayD,
-        coarse=rasterCoarse,
-        fine=rasterFine,
-        offsetRaster=offsetRaster
-      )
-      
-      if(!is.null(thisBlock)) {
-        partHere = dimnames(thisBlock)[[1]]
-        matchPartHere = match(partHere, Spartitions)
-        
-        theOrder = order(matchPartHere)
-        
-        haveWritten = FALSE
-        writeCounter1 = 0
-        while( (!haveWritten) & (writeCounter1 < 20) ) {
-          haveWritten = tryCatch(
-            spatial.tools::binary_image_write(
-              filename = smoothingRaster,
-              mode=theType,
-              image_dims = dim(smoothingRasterTemplate),
-              data=as.double(thisBlock[theOrder,theOrder,]), 
-              data_position = list(
-                matchPartHere[theOrder], 
-                matchPartHere[theOrder], 
-                layerSeq
-              )
-            ), error = function(err) {warning(err);-1}  )
-          haveWritten = (haveWritten != -1)
-          writeCounter1 = writeCounter1 + 1
+        thisBlock = try(smoothingMatrixEntries(cell1 = Dcell1,
+            focalList=kernelArrayD,
+            coarse=rasterCoarse,
+            fine=rasterFine,
+            offsetRaster=offsetRaster
+        ))
+        if(class(thisBlock) == 'try-error') {
+          warning(paste("Error in smoothingMatrixEntries", Dcell1))
+          thisBlock = NA 
         }
-      } # end not null
-      dim(thisBlock)
-    } # end foreach loop
+        
+        if(!is.null(thisBlock)) {
+          partHere = dimnames(thisBlock)[[1]]
+          matchPartHere = match(partHere, Spartitions)
+          
+          theOrder = order(matchPartHere)
+          
+          haveWritten = FALSE
+          writeCounter1 = 0
+          while( (!haveWritten) & (writeCounter1 < 20) ) {
+            haveWrittenFirst = tryCatch(
+                spatial.tools::binary_image_write(
+                    filename = smoothingRaster,
+                    mode=theType,
+                    image_dims = dim(smoothingRasterTemplate),
+                    data=as.double(thisBlock[theOrder,theOrder,]), 
+                    data_position = list(
+                        matchPartHere[theOrder], 
+                        matchPartHere[theOrder], 
+                        layerSeq
+                    )
+                ), error = function(err) {warning(err);-1}  )
+            haveWritten = !identical(haveWrittenFirst, -1)
+            if(!haveWritten & verbose){
+              warning(paste("error in write attempt", writeCounter1, "of cell", Dcell1, haveWrittenFirst))
+            }
+            writeCounter1 = writeCounter1 + 1
+          }
+        } # end not null
+        dim(thisBlock)
+      } # end foreach loop
   
   cellsWithData = which(unlist(lapply(diagBlocks, length))>0)
   
@@ -322,11 +331,11 @@ smoothingMatrixDiag = function(
   allCells = expand.grid(cellsWithData, cellsWithData)
   colnames(allCells) = gsub("^Var", "cell", colnames(allCells))
   allCells= cbind(allCells,
-    minCell = pmin(allCells[,'cell2'],allCells[,'cell1']),
-    maxCell = pmax(allCells[,'cell2'],allCells[,'cell1'])
+      minCell = pmin(allCells[,'cell2'],allCells[,'cell1']),
+      maxCell = pmax(allCells[,'cell2'],allCells[,'cell1'])
   )
   allCells = cbind(allCells,
-    cellPair=1i*allCells[,'minCell'] + allCells[,'maxCell'])
+      cellPair=1i*allCells[,'minCell'] + allCells[,'maxCell'])
   
   # row, col of cell1
   toAdd = rowColFromCell(rasterCoarse,allCells[,'cell1'])
@@ -348,16 +357,16 @@ smoothingMatrixDiag = function(
   
   # col dist
   allCells = cbind(allCells,
-    colDiff = allCells[,'col2'] -allCells[,'col1'],
-    rowDiff = allCells[,'row1'] -allCells[,'row2'],
-    colDiffUnique = allCells[,'colmax'] -allCells[,'colmin'],
-    rowDiffUnique = allCells[,'rowmin'] -allCells[,'rowmax']
+      colDiff = allCells[,'col2'] -allCells[,'col1'],
+      rowDiff = allCells[,'row1'] -allCells[,'row2'],
+      colDiffUnique = allCells[,'colmax'] -allCells[,'colmin'],
+      rowDiffUnique = allCells[,'rowmin'] -allCells[,'rowmax']
   )
   
   # dist pair
   allCells = cbind(allCells,
-    dist=1i*allCells[,'rowDiff'] + allCells[,'colDiff'],
-    distUnique = 1i*allCells[,'rowDiffUnique'] + allCells[,'colDiffUnique']
+      dist=1i*allCells[,'rowDiff'] + allCells[,'colDiff'],
+      distUnique = 1i*allCells[,'rowDiffUnique'] + allCells[,'colDiffUnique']
   )
   # get rid of diagonals
   allCells = allCells[allCells[,'dist']!= 0,]
@@ -365,21 +374,21 @@ smoothingMatrixDiag = function(
   
   # unique distance in first quadrant, x dist bigger than y dists
   allCells = cbind(allCells,
-    distOf8 =
-      pmax(abs(Re(allCells[,'dist'])), abs(Re(allCells[,'dist']))) +
-      1i * pmin(abs(Im(allCells[,'dist'])), abs(Im(allCells[,'dist']))),
-    posY = (Im(allCells[,'dist'])>0)+1,
-    YgtX = (abs(Im(allCells[,'dist'])) > abs(Re(allCells[,'dist'])) )+1
+      distOf8 =
+          pmax(abs(Re(allCells[,'dist'])), abs(Re(allCells[,'dist']))) +
+          1i * pmin(abs(Im(allCells[,'dist'])), abs(Im(allCells[,'dist']))),
+      posY = (Im(allCells[,'dist'])>0)+1,
+      YgtX = (abs(Im(allCells[,'dist'])) > abs(Re(allCells[,'dist'])) )+1
   )
   
   # find the extent of the kernel with the biggest bandwidth
   maxDist = max(unlist(lapply(focalList$focal, dim)))
   # how many coarse cells it is
   maxCoarseCells = maxDist /
-    max(ceiling(res(rasterCoarse)/res(rasterFine)))
+      max(ceiling(res(rasterCoarse)/res(rasterFine)))
   # find pairs of coarse cells separated by a distance bigger than this
   cellsAllZeros = pmax(abs(Re(allCells[,'dist'])),
-    abs(Im(allCells[,'dist']))) > maxCoarseCells
+      abs(Im(allCells[,'dist']))) > maxCoarseCells
   
   allCells =allCells[!cellsAllZeros,]
   allCells = allCells[order(allCells[,'colDiff'], allCells[,'rowDiff']),]
@@ -388,27 +397,27 @@ smoothingMatrixDiag = function(
   outArray = brick(smoothingRaster)
   names(outArray) = Sbw
   list(
-    smoothingArray = outArray,
-    partitions = Spartitions,
-    bw = Sbw,
-    cells=allCells,
-    uniqueDist = unique(allCells$dist))
+      smoothingArray = outArray,
+      partitions = Spartitions,
+      bw = Sbw,
+      cells=allCells,
+      uniqueDist = unique(allCells$dist))
 }
 
 # Computes all of the off-diagonal entries of the smoothing matrix for the partitions
 # Specifically, the computations are done for neighbouring partitions
 smoothingMatrixOneDist = function(
-  x, allCells,
-  focal, coarse,
-  fine, offsetRaster
+    x, allCells,
+    focal, coarse,
+    fine, offsetRaster
 ) {
   kernelArray = kernMat(
-    cellDist=c(Re(x), Im(x)),
-    focalList=focal,
-    coarse=coarse,
-    fineRelativeRes=fine,
-    reorder=TRUE,
-    xv = names(offsetRaster))
+      cellDist=c(Re(x), Im(x)),
+      focalList=focal,
+      coarse=coarse,
+      fineRelativeRes=fine,
+      reorder=TRUE,
+      xv = names(offsetRaster))
   
   
   whichCells = allCells[which(allCells[,'dist']==x),]
@@ -430,19 +439,19 @@ smoothingMatrixOneDist = function(
   res = list()
   for(Dcell1 in unique(whichCellsD[,'cell1'])){
     Scell2 = whichCellsD[
-      whichCellsD[,'cell1']==Dcell1,
-      'cell2'
+        whichCellsD[,'cell1']==Dcell1,
+        'cell2'
     ]
     
     res[[as.character(Dcell1)]] =
-      smoothingMatrixEntries(
-        cell1=Dcell1,
-        cell2 = Scell2,
-        focalList=kernelArray,
-        coarse=coarse,
-        fine=fine,
-        offsetRaster=offsetRaster
-      )
+        smoothingMatrixEntries(
+            cell1=Dcell1,
+            cell2 = Scell2,
+            focalList=kernelArray,
+            coarse=coarse,
+            fine=fine,
+            offsetRaster=offsetRaster
+        )
   }
   res
 }
