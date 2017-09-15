aae0142f2f6532b6b572bde686c756b8ea06e387
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-09-15 19:46:23 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/localem@259 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/localem/R/finalSmooth.R
@@ -0,0 +1,42 @@
+
+# Computes the risk estimation to the fine raster by smoothing the risk of the partitions at the final iteration
+finalSmooth = function(
+    x, 
+    Slayers, 
+    ncores = 1, 
+    filename) {
+  
+  toSmooth = x$riskAll
+  levels(toSmooth)[[1]] = levels(toSmooth)[[1]][, c("ID", Slayers)]
+  
+  deratifyFile = tempfile("deratify", tempdir(), ".grd")
+  
+  toSmooth = deratify(toSmooth, filename = deratifyFile)
+  
+  endCluster = FALSE
+  theCluster = NULL
+  if(length(grep("cluster", class(ncores))) ) {
+    theCluster = ncores
+  } else if(!is.null(ncores)) {
+    if(ncores > 1) {
+      theCluster = parallel::makeCluster(spec=ncores, type='PSOCK', methods=TRUE)
+      parallel::setDefaultCluster(theCluster)
+      endCluster = TRUE
+    }
+  }
+  
+  xOrig = x
+  result = focalMult(
+      x=toSmooth, 
+      w=xOrig$smoothingMatrix$focal$focal, 
+      edgeCorrect = TRUE,
+      filename = filename,
+      cl = theCluster
+  )
+  
+  if(endCluster)  
+    parallel::stopCluster(theCluster)
+  
+  return(result)
+  
+}
