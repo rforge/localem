021194c63f45d0cb3fbd84b72e1ad26a50cdb658
patrick <patrick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-10-31 19:02:09 +0000

    parallel raster partitions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/localem@371 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/localem/R/rasterRgcp.R
+++ pkg/localem/R/rasterRgcp.R
@@ -3,7 +3,8 @@ rasterPartitionRgcp = function(
 	coarsePolyList,
 	finePolyList,
 	Ncoarse, Nfine,
-	pathBase, verbose=FALSE
+	pathBase, verbose=FALSE,
+	mc.cores=1
 	) {
 
 	# add an X before integers in names
@@ -17,13 +18,12 @@ rasterPartitionRgcp = function(
 			"X\\1", names(finePolyList))
 
 
+	Dmap = names(coarsePolyList)[1]
 
-	rasterList = list()
-
-	DmapFirst = Dmap = names(coarsePolyList)[1]
-
-	if(verbose) cat(Dmap, ' ')
-	rasterList[[ Dmap ]] = 
+	if(verbose) {
+		cat('first rasterization ')
+	}
+	rasterList1 = 
 	localEM::rasterPartition(
 		polyCoarse = coarsePolyList[[Dmap]],
 		polyFine = finePolyList[[Dmap]],
@@ -33,60 +33,89 @@ rasterPartitionRgcp = function(
 		path=file.path(pathBase, Dmap),
 		verbose=verbose
 		)
-	fineCells = deratify(rasterList[[DmapFirst]]$rasterFine, 'cellCoarse')
-	coarseCells = rasterList[[DmapFirst]]$rasterCoarse
+	fineCells = deratify(rasterList1$rasterFine, 'cellCoarse')
+	coarseCells = rasterList1$rasterCoarse
 
-	for(Dmap in names(coarsePolyList)[-1]) {
-		if(verbose) cat(Dmap, ' ')
-		rasterList[[ Dmap ]] = 
-		localEM::rasterPartition(
-			polyCoarse = coarsePolyList[[Dmap]],
-			polyFine = finePolyList[[Dmap]],
-			cellsCoarse = coarseCells[['cellCoarse']],
-			cellsFine = fineCells,
-			NULL, NULL, NULL,
-			path=file.path(pathBase, Dmap),
-			verbose=verbose>1)
+	Smap = setdiff(names(coarsePolyList)[-1], Dmap)
+	MoreArgs = list(
+		cellsCoarse = coarseCells[['cellCoarse']],
+		cellsFine = fineCells,
+		bw=NULL, focalSize=NULL, xv=NULL,
+		verbose=verbose
+	)
+
+	if(verbose) {
+		cat('remaining rasterizations ')
 	}
 
+	if(mc.cores <= 1) {
+		cl = NULL
+		rasterList2 = Map(localEM::rasterPartition,
+			polyCoarse = coarsePolyList[Smap],
+			polyFine = finePolyList[Smap],
+			path=file.path(pathBase, Dmap),
+			MoreArgs = MoreArgs
+			)
+	} else {
+		cl = parallel::makeCluster(mc.cores, type='PSOCK', methods=TRUE)
+		parallel::clusterEvalQ(cl, require('raster'))
+		parallel::clusterEvalQ(cl, require('Matrix'))
+
+		rasterList2 = parallel::clusterMap(
+			cl = cl,
+			fun=localEM::rasterPartition,
+			polyCoarse = coarsePolyList[Smap],
+			polyFine = finePolyList[Smap],
+			path=file.path(pathBase, Smap),
+			MoreArgs = MoreArgs
+			)
+	}
+
+	if(FALSE) { # for debugging
+		rasterList2 = list()
+		for(DDmap in Smap)
+			rasterList2[[DDmap]] = localEM::rasterPartition(
+			polyCoarse = coarsePolyList[[DDmap]],
+			polyFine = finePolyList[[DDmap]],
+			path=file.path(pathBase, DDmap),
+		cellsCoarse = coarseCells[['cellCoarse']],
+		cellsFine = fineCells,
+		bw=NULL, focalSize=NULL, xv=NULL,
+		verbose=verbose
+				)
+	}
+
+	rasterList = c(
+		list(x=rasterList1),
+		rasterList2
+		)
+
 	toBrick = lapply(rasterList, function(xx) xx$offset[['offset']])
-	names(toBrick)[1] = 'x'
 	offsetStack = do.call(raster::stack, toBrick)
 	offsetStack = offsetStack * prod(res(offsetStack))
-	names(offsetStack) = names(rasterList)
+	names(offsetStack) = names(rasterList) = c(Dmap, Smap)
 
 # sum of offsets in intersection of
 # coarse polygons and coarse cells
-	Oijl = list()
-	if(verbose) cat('\n')
+	if(verbose) {
+		cat(' intersections ')
+	}
 
-	for(Dmap in names(rasterList)) {
-		if(verbose) cat(Dmap, ' ')
-
-		stuff = deratify(rasterList[[Dmap]]$rasterFine, 
-			c('idCoarse','cellCoarse'))
-		notNa= which(!is.na(values(offsetStack[[Dmap]])))
-		offsetSumHere = cbind(
-			offset=values(offsetStack[[Dmap]])[notNa], 
-			as.data.frame(deratify(rasterList[[Dmap]]$rasterFine, 
-				c('idCoarse','cellCoarse')))[notNa,])
-		offsetAggHere = aggregate(
-			offsetSumHere[,'offset'],
-			offsetSumHere[,c('idCoarse','cellCoarse')],
-			sum, na.rm=TRUE)
-		offsetAggHere=offsetAggHere[offsetAggHere$x > 0, ]
-
-		Oijl[[Dmap]] = sparseMatrix(
-			offsetAggHere[,'cellCoarse'],
-			offsetAggHere[,'idCoarse'],
-			x = offsetAggHere[,'x'],
-			dim = c(
-				ncell(coarseCells),
-				length(rasterList[[Dmap]]$polyCoarse)),
-			dimnames = list(
-				values(coarseCells),
-				rasterList[[Dmap]]$polyCoarse$idCoarse
-				)
+	if(!is.null(cl)) {
+		Oijl = parallel::clusterMap(
+			cl = cl,
+			fun=rgcpOijlFun,
+			rasterHere = rasterList,
+			offsetStackHere = as.list(offsetStack),
+			MoreArgs = list(coarseCells = coarseCells)
+			)
+		parallel::stopCluster(cl)
+	} else {
+		Oijl = Map(
+			fun=rgcpOijlFun,
+			rasterHere = rasterList,
+			offsetStackHere = as.list(offsetStack),
+			MoreArgs = list(coarseCells = coarseCells)
 			)
 	}
 
@@ -94,9 +123,41 @@ rasterPartitionRgcp = function(
 	offsetL = apply(do.call(cbind, offsetIL), 1, sum)
 	offsetMat = Matrix::Diagonal(length(offsetL), offsetL)
 
+	if(verbose) {
+		cat('\n')
+	}
+	
 	list(
 		Oijl = Oijl,
 		cells =  list(coarse=coarseCells, fine=fineCells),
 		offset = list(raster = offsetStack, matrix = offsetMat)
+		)
+}
+
+
+rgcpOijlFun = function(rasterHere, offsetStackHere, coarseCells) {
+	stuff = raster::deratify(rasterHere$rasterFine, 
+		c('idCoarse','cellCoarse'))
+	notNa= which(!is.na(raster::values(offsetStackHere)))
+	offsetSumHere = cbind(
+		offset=raster::values(offsetStackHere)[notNa], 
+		as.data.frame(raster::deratify(rasterHere$rasterFine, 
+			c('idCoarse','cellCoarse')))[notNa,])
+	offsetAggHere = aggregate(
+		offsetSumHere[,'offset'],
+		offsetSumHere[,c('idCoarse','cellCoarse')],
+		sum, na.rm=TRUE)
+	offsetAggHere=offsetAggHere[offsetAggHere$x > 0, ]
+	Matrix::sparseMatrix(
+		offsetAggHere[,'cellCoarse'],
+		offsetAggHere[,'idCoarse'],
+		x = offsetAggHere[,'x'],
+		dim = c(
+			raster::ncell(coarseCells),
+			length(rasterHere$polyCoarse)),
+		dimnames = list(
+			raster::values(coarseCells),
+			rasterHere$polyCoarse$idCoarse
+			)
 		)
 }
